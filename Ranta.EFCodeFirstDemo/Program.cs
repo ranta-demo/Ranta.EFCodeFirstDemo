380a8c2ee9e4e13566896abb818dc80cbe11a2ba
HarrisZhang <harriszhang@live.cn>
2017-06-10 10:24:06 +0800

    update

--- Ranta.EFCodeFirstDemo/Program.cs
+++ Ranta.EFCodeFirstDemo/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Ranta.EFCodeFirstDemo.Repository;
+using Ranta.EFCodeFirstDemo.Repository.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,24 @@ namespace Ranta.EFCodeFirstDemo
     {
         static void Main(string[] args)
         {
+            using (var demoDB = new RantaDemoDBContext())
+            {
+                var name = Console.ReadLine();
+
+                var student = new Student { Name = name };
+                demoDB.Students.Add(student);
+                demoDB.SaveChanges();
+
+                var query = from b in demoDB.Students orderby b.Name select b;
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.Read();
+            }
         }
     }
 }
---
+++ Ranta.EFCodeFirstDemo/Repository/RantaDemoDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Ranta.EFCodeFirstDemo.Repository.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranta.EFCodeFirstDemo.Repository
+{
+    public class RantaDemoDBContext : DbContext
+    {
+        public RantaDemoDBContext() : base("RantaDemoDB")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
